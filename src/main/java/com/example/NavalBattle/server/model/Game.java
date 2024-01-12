eebe2d5cff5d5a323e681ad0b04d8340fb8d1d74
MOY Zacharie <moy@et.esiea.fr>
2024-01-12 11:48:32 +0100

    started coding placeShips

--- src/main/java/com/example/NavalBattle/server/model/Game.java
+++ src/main/java/com/example/NavalBattle/server/model/Game.java
@@ -1,8 +1,11 @@
 // Game.java
 package com.example.NavalBattle.server.model;
 
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.thymeleaf.standard.expression.Each;
 
 public class Game {
     private final String gameId;
@@ -33,6 +36,24 @@ public class Game {
     private void placeShips() {
         // You need to implement the logic for placing ships on the grid
         // based on the game rules (size, orientation, no overlapping, etc.)
+        // Aircraft carrier: 1* 4 conscutive squares
+        // Cruisers: 2* Each occupies 3 squares
+        // Destroyers: 3* Each occupies 2 squares
+        // Torpedo Boats: 4* Each occupies 1 square
+        int min = 1 ;
+        int max = 10;
+        int randomRow[] = new int[10];
+        for (int i=0; i<10; i++){
+            randomRow[i] = 0;
+        }
+        int randomColumn[] = new int[10];
+        for (int i=0; i<10; i++){
+            randomColumn[i] = 0;
+        }
+        randomColumn[0] = ThreadLocalRandom.current().nextInt(min, max + 1);
+        randomColumn[0] = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+        //Conversion to to char from int : (char)(a+64) (1 becomes A, 2 becomes B, ...)
     }
 
     public String getGameId() {
